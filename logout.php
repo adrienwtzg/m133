c26e05a43f0cdb517ec74bad8d9225d2e7ea1af9
Toucanite <LUCAS.DNT@c314se01-3.local>
2016-11-22 10:33:02 +0100

    merge

--- logout.php
+++ logout.php
@@ -1,13 +1,7 @@
 <?php
     session_start(); // Démarre la session
-    
-    $login = $_SESSION["login"]; // Récupère la variable login de la session
-    $pass = $_SESSION["pass"]; // Récupère la variable pass de la session
-    
-    if ($login == "" && $pass == "") {  // Vérifie qu le login et le pass sient remplis
-        header("location: /formulaire.php"); // Redirige vers le fomurlaire 
-        exit();
-    }
+    session_destroy();
+    header("Location: /formulaire.php");
 ?>
 <!DOCTYPE html>
 <html>
@@ -17,14 +11,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
-        <pre>
-            <?php
-                echo "<h1>Bienvenue " . $login . " !</h1>";
-            ?>
-        </pre>
-        <form method="POST" action="formulaire.php">
-            <input type="submit" value="Log Out" />
-        </form>
+        
     </body>
 </html>
 

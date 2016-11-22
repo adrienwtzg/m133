45af390e05e5ef382d2567b3ad256dc155bcb737
adrienwtzg <ADRIEN.WTZG@c314se01-100.local>
2016-11-22 10:19:41 +0100

    aaa

---
+++ Acceuil.php
@@ -0,0 +1,29 @@
+<?php
+    session_start(); // Démarre la session
+    
+    $login = $_SESSION["login"]; // Récupère la variable login de la session
+    $pass = $_SESSION["pass"]; // Récupère la variable pass de la session
+    
+    if ($login == "" && $pass == "") {  // Vérifie qu le login et le pass sient remplis
+        header("location: /formulaire.php"); // Redirige vers le fomurlaire 
+        exit();
+    }
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Admin Page</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <pre>
+            <?php
+                echo "<h1>Bienvenue " . $login . " !</h1>";
+            ?>
+        </pre>
+        <form method="POST" action="logout.php">
+            <input type="submit" value="Log Out" />
+        </form>
+    </body>
+</html>
---
+++ formulaire.php
@@ -0,0 +1,72 @@
+<?php
+    session_start(); // Démarre la session
+    
+    $passAdmin = "7e240de74fb1ed08fa08d38063f6a6a91462a815"; // Mot de passe de l'admin crypté en sha1 "aaa"
+    $login = (isset($_REQUEST["login"])?$_REQUEST["login"]:""); // Stockage de la réponse de l'utilisateur pour le login
+    $pass = (isset($_REQUEST["pass"])?sha1($_REQUEST["pass"]):""); // Stockage de la réponse de l'utilisateur pour le pass
+    $_SESSION["login"] = $login; // Assignation du login dans la session
+    $_SESSION["pass"] = $pass; // Assignation du login dans la session
+    
+    $MessageErreurLogin = "";
+    $MessageErreurPass = "";
+    
+    if ((!isset($login)) || (!isset($pass))) {
+        $MessageErreurLogin = "";
+        $MessageErreurPass = "";
+    }
+    else
+    {
+       if (($login == "admin") && ($pass == $passAdmin)) {  // Condition de mot de passe
+            header("Location: /Acceuil.php"); // Redirige vers la page d'acceuil de l'admin
+            exit();
+        }
+        elseif ($login != "admin")
+        {
+            $MessageErreurLogin = "Login incorrect !";
+        }
+        elseif (($login == "admin") && ($pass != $passAdmin))
+        {
+            $MessageErreurPass = "Mot de passe incorrect !";
+        } 
+    } 
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Formulaire</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <?php
+            echo $MessageErreurLogin;
+            echo $MessageErreurPass;
+        ?>
+        <form action="#" method="POST">
+            <table>
+                <tr>
+                    <td>
+                        Login :
+                    </td>
+                    <td>
+                        <input type="text" name="login" value="<?php echo $login ?>"/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Password :
+                    </td>
+                    <td>
+                        <input type="password" name="pass"/>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <input type="submit" value="Login" name="btnSubmit"/>
+                    </td>    
+                </tr>
+            </table>
+        </form>
+    </body>
+</html>

5f39bcf1776ccf3943d5db2a3a3a30b4536c1c96
adrienwtzg <ADRIEN.WTZG@10.134.181.100>
2016-11-29 10:33:24 +0100

    asdasdasd

--- TablePHP.php
+++ TablePHP.php
@@ -1,4 +1,9 @@
-<?php ?>
+<?php
+    require("Library.inc.php");
+
+    $LIGNES = (isset($_REQUEST["Lignes"]) ? $_REQUEST["Lignes"] : "0");
+    $COLONNES = (isset($_REQUEST["Colonnes"]) ? $_REQUEST["Colonnes"] : "0");
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -30,30 +35,14 @@
     <body>
         <div>
             <form>
-                Lignes : <input type="number" name="Lignes" value=""/>
-                Colonnes : <input type="number" name="Colonnes" value=""/>
+                Lignes : <input type="number" name="Lignes" value="<?php echo $LIGNES?>"/>
+                Colonnes : <input type="number" name="Colonnes" value="<?php echo $COLONNES ?>"/>
+                Couleur du tableau : 
                 <input type="submit" value="Construire" name="btnSubmit"/>
                 </br>
             </form>
             <?php
-            $LIGNES = (isset($_REQUEST["Lignes"]) ? $_REQUEST["Lignes"] : "0");
-            $COLONNES = (isset($_REQUEST["Colonnes"]) ? $_REQUEST["Colonnes"] : "0");
-
-            define("LIGNES", $LIGNES);
-            define("COLONNES", $COLONNES);
-
-            if (($LIGNES > 0) && ($COLONNES > 0)) {
-                echo "\n\t<table border='1px, black, solid' borderCollapse='collapse'>";
-
-                for ($l = 0; $l < LIGNES; $l++) {
-                    echo "\n\t\t<tr>";
-                    for ($i = 0; $i < COLONNES; $i++) {
-                        echo "\n\t\t\t<td>" . ($l * COLONNES + $i + 1) . "</td>";
-                    }
-                    echo "\n\t\t</tr>";
-                }
-                echo "\n\t</table>";
-            }
+                AfficheTableau($LIGNES, $COLONNES);
             ?>
     </body>
 </html>

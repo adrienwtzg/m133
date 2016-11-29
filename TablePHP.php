2e2aafdd0427055d1caa0d3bb0c93cd1543f59a3
adrienwtzg <ADRIEN.WTZG@10.134.181.100>
2016-11-29 08:24:52 +0100

    table

---
+++ TablePHP.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <?php
+            $LIGNES = 3;
+            $COLLONES = 4;
+            
+            for($l = 0; $l < $LIGNES; $l++)
+            {
+                for($l = 0; $l < $COLLONES; $l++)
+                {
+                    
+                }
+            }
+        ?>
+        <div>
+        </div>
+    </body>
+</html>

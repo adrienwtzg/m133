d3ecc98cdb37dd9ffe3577533c6e640a133256b9
Toucanite <LUCAS.DNT@c314se01-3.local>
2016-11-29 10:38:28 +0100

    Erreur de merle supprimer

--- TablePHP.php
+++ TablePHP.php
@@ -1,13 +1,8 @@
 <?php
-<<<<<<< HEAD
-$LIGNES = (isset($_REQUEST["Lignes"]) ? $_REQUEST["Lignes"] : "0");
-$COLONNES = (isset($_REQUEST["Colonnes"]) ? $_REQUEST["Colonnes"] : "0");
-=======
     require("Library.inc.php");
 
     $LIGNES = (isset($_REQUEST["Lignes"]) ? $_REQUEST["Lignes"] : "0");
     $COLONNES = (isset($_REQUEST["Colonnes"]) ? $_REQUEST["Colonnes"] : "0");
->>>>>>> Adrien_Branche
 ?>
 <!DOCTYPE html>
 <html>
@@ -40,41 +35,14 @@ $COLONNES = (isset($_REQUEST["Colonnes"]) ? $_REQUEST["Colonnes"] : "0");
     <body>
         <div>
             <form>
-<<<<<<< HEAD
                 Lignes : <input type="number" name="Lignes" value="<?php echo $LIGNES ?>"/>
                 Colonnes : <input type="number" name="Colonnes" value="<?php echo $COLONNES ?>"/>
-=======
-                Lignes : <input type="number" name="Lignes" value="<?php echo $LIGNES?>"/>
-                Colonnes : <input type="number" name="Colonnes" value="<?php echo $COLONNES ?>"/>
                 Couleur du tableau : 
->>>>>>> Adrien_Branche
                 <input type="submit" value="Construire" name="btnSubmit"/>
                 </br>
             </form>
             <?php
-<<<<<<< HEAD
-            define("LIGNES", $LIGNES);
-            define("COLONNES", $COLONNES);
-
-            if (($LIGNES > 0) && ($COLONNES > 0)) {
-                echo "\n\t<table border='1px, black, solid' borderCollapse='collapse'>";
-
-                for ($l = 0; $l < LIGNES; $l++) {
-                    echo "\n\t\t<tr>";
-                    for ($i = 0; $i < COLONNES; $i++) {
-                        if ($l == 0) {
-                            echo "\n\t\t\t<th>" . ($l * COLONNES + $i + 1) . "</th>";
-                        } else {
-                            echo "\n\t\t\t<td>" . ($l * COLONNES + $i + 1) . "</td>";
-                        }
-                    }
-                    echo "\n\t\t</tr>";
-                }
-                echo "\n\t</table>";
-            }
-=======
                 AfficheTableau($LIGNES, $COLONNES);
->>>>>>> Adrien_Branche
             ?>
     </body>
 </html>

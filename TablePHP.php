d548206d7256f9d76498af6537b4f14b048431bd
Toucanite <LUCAS.DNT@c314se01-3.local>
2016-11-29 08:44:33 +0100

    Correction Erreur Merge

--- TablePHP.php
+++ TablePHP.php
@@ -18,17 +18,7 @@
         </style>
     </head>
     <body>
-<<<<<<< HEAD
         <div>
-            <?php
-            $LIGNES = 3;
-            $COLONNES = 4;
-
-            for ($l = 0; $l < $LIGNES; $l++) {
-                echo "";
-                for ($l = 0; $l < $COLONNES; $l++) {
-                    
-=======
         <?php
             define("LIGNES", 3);
             define("COLLONES", 4);
@@ -41,18 +31,12 @@
                 for($i = 0; $i < COLLONES; $i++)
                 {
                     echo "\n\t\t\t<td>" . ($l * COLLONES + $i + 1) . "</td>";
->>>>>>> Adrien_Branche
                 }
                 echo "\n\t\t</tr>";
             }
-<<<<<<< HEAD
-            ?>
-=======
-            
             echo "\n\t</table>";
         ?>
         <div>
->>>>>>> Adrien_Branche
         </div>
     </body>
 </html>

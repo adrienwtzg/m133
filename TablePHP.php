abbdcc10ead0308da1dd541d6e845435cfb58820
Toucanite <LUCAS.DNT@c314se01-3.local>
2016-11-29 08:42:35 +0100

    AjoutStyle

--- TablePHP.php
+++ TablePHP.php
@@ -1,20 +1,31 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title></title>
+        <title>Tableau en PHP</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <style>
+            table{
+                border-style: solid;
+                border-color: #000000;
+                border-width: 1px;
+                color: #000000;
+            }
+            th{
+                background-color: #AAAAAA;
+                color: #FFFFFF;
+            }
+        </style>
     </head>
     <body>
         <div>
             <?php
             $LIGNES = 3;
-            $COLLONES = 4;
-            
-            for($l = 0; $l < $LIGNES; $l++)
-            {
-                for($l = 0; $l < $COLLONES; $l++)
-                {
+            $COLONNES = 4;
+
+            for ($l = 0; $l < $LIGNES; $l++) {
+                echo "";
+                for ($l = 0; $l < $COLONNES; $l++) {
                     
                 }
             }
